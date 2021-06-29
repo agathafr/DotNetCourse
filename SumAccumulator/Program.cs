ed68ba2afd2cc991609358caf694ef9c42d6e7b4
Agatha França <agathafranca@outlook.com>
2021-06-29 12:17:38 -0300

    uploading new project

---
+++ SumAccumulator/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumAccumulator
+{
+    class Program
+    {
+        static int[] numbers = { 45, 78, 56, 23, 41, 64, 57 };
+        static void Main(string[] args)
+        {
+            Sum(numbers);
+            Output(numbers);
+
+            Console.ReadKey();
+        }
+        static int Sum(int[] numbers)
+        {
+            int sum = 0;
+            foreach (var num in numbers)
+            {
+                sum += num;
+            }
+            return sum;
+        }
+        static void Output(int[] numbers)
+        {
+            int result = Sum(numbers);
+            Console.WriteLine(result);
+        }
+    }
+
+   
+}
