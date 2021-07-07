0fe9dae0ad03f2eafeef725beba1e60e7f7c017c
Agatha França <agathafranca@outlook.com>
2021-07-07 15:45:10 -0300

    adding a new project

---
+++ DaysOfTheWeek/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DaysOfTheWeek
+{
+    class Program
+    {
+        // declaração de variáveis
+        static int escolha;
+        static string dia;
+        static void Main(string[] args)
+        {
+            RecebeDado();
+            IdentificaDiaDaSemana(escolha);
+            ExibeSaida(escolha);
+
+            Console.ReadKey();
+        }
+        // entrada de dados
+        static void RecebeDado()
+        {
+            Console.WriteLine("Digite um número de 1 a 7 para ver o dia da semana correspondente a ele: ");
+            escolha = int.Parse(Console.ReadLine());
+            Console.Clear();
+        }
+        // processamento
+        static string IdentificaDiaDaSemana(int escolha)
+        {
+            switch (escolha)
+            {
+                case 1:
+                    dia = "Segunda";
+                    break;
+                case 2:
+                    dia = "Terça";
+                    break;
+                case 3:
+                    dia = "Quarta";
+                    break;
+                case 4:
+                    dia = "Quinta";
+                    break;
+                case 5:
+                    dia = "Sexta";
+                    break;
+                case 6:
+                    dia = "Sábado";
+                    break;
+                case 7:
+                    dia = "Domingo";
+                    break;
+                default:
+                    Console.WriteLine("Informe um número de 1 a 7!");
+                    break;
+            }
+            return dia;
+        }
+        // saída de dados
+        static void ExibeSaida(int escolha)
+        {
+            Console.WriteLine("Dia da semana: " + IdentificaDiaDaSemana(escolha));
+        }
+    }
+}
