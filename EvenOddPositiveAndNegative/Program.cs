c9a4ca6db4f9e75d675d3927294a2ae0f5514478
Agatha França <agathafranca@outlook.com>
2021-07-13 08:22:58 -0300

    adding a new project

---
+++ EvenOddPositiveAndNegative/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOddPositiveAndNegative
+{
+    class Program
+    {
+        static int[] numbers = new int[5];
+        
+        static void Main(string[] args)
+        {
+            ReceivesData();
+            DisplaysOutput();
+
+            Console.ReadKey();
+        }
+
+        static void ReceivesData()
+        {
+            Console.WriteLine("Enter five values: ");
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = int.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            }
+        }
+
+        static int CountsEvenValues()
+        {
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] % 2 == 0)
+                    count++;
+            }
+
+            return count;
+        }
+
+        static int CountsOddValues()
+        {
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (Math.Abs(numbers[i]) % 2 == 1)
+                    count++;
+            }
+
+            return count;
+        }
+
+        static int CountsPositiveValues()
+        {
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] > 0)
+                    count++;
+            }
+
+            return count;
+        }
+
+        static int CountsNegativeValues()
+        {
+            int count = 0;
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (numbers[i] < 0)
+                    count++;
+            }
+
+            return count;
+        }
+
+        static void DisplaysOutput()
+        {
+            Console.WriteLine(CountsEvenValues() + " valor(es) par(es)");
+            Console.WriteLine(CountsOddValues() + " valor(es) ímpar(es)");
+            Console.WriteLine(CountsPositiveValues() + " valor(es) positivo(s)");
+            Console.WriteLine(CountsNegativeValues() +  " valor(es) negativo(s)");
+        }
+    }
+}
---
+++ SixConsecutiveOddNumbers/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SixConsecutiveOddNumbers
+{
+    class Program
+    {
+        static int number;
+        static int[] numbers = new int[6];
+
+        static void Main(string[] args)
+        {
+            ReceivesData();
+            FindsSixConsecutiveOddNumbers();
+            DisplaysOutput();
+
+            Console.ReadKey();
+        }
+
+        static void ReceivesData()
+        {
+            Console.WriteLine("Enter a positive integer value: ");
+            number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0)
+                number++;
+
+            Console.Clear();
+        }
+        static void FindsSixConsecutiveOddNumbers()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                numbers[i] = number;
+                number += 2;
+            }
+        }
+
+        static void DisplaysOutput()
+        {
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.WriteLine(numbers[i]);
+            }
+        }
+    }
+}
