d7b6a25e81af81fa138382fe88cd298e54f9be3f
Agatha França <agathafranca@outlook.com>
2021-07-26 07:29:44 -0300

    adding a new project

---
+++ SimpleProduct/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleProduct
+{
+    class Program
+    {
+        static int numberOne;
+        static int numberTwo;
+        static int prod;
+        static void Main(string[] args)
+        {
+            ReceivesData();
+            ReturnsTheProduct();
+            DisplaysOutput();
+
+            Console.ReadKey();
+        }
+
+        static void ReceivesData()
+        {
+            Console.WriteLine("Enter an integer: ");
+            numberOne = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter another integer: ");
+            numberTwo = int.Parse(Console.ReadLine());
+
+            Console.Clear();
+        }
+
+        static int ReturnsTheProduct()
+        {
+            prod = numberOne * numberTwo;
+            return prod;
+        }
+
+        static void DisplaysOutput()
+        {
+            Console.WriteLine("PROD = " + prod);
+        }
+    }
+}
